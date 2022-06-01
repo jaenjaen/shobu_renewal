b798162d4ac5e844ce47f7a2b5b10078bfeb8453
Jaeeun Lee <je028770@gmail.com>
2022-06-01 22:33:00 +0900

    create main module

---
+++ main/src/test/java/com/shobu/information/InformationApplicationTests.java
@@ -0,0 +1,14 @@
+package com.shobu.information;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
